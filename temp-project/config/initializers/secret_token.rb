e70c7db6085864775f92a60a3144e123a94ff1ef
dmaximus <dmitry.komissar@gmail.com>
2012-09-04 19:47:23 +0300

    first commit

---
+++ temp-project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempProject::Application.config.secret_token = 'fcb123a7b120a9bcf1d1a746c1916675631d3fe798fd4fda90df6de0c56d7f232ca7864c80f5126c9e237db35f041faa752b5f0ed8d5f747393808e197f25eee'
